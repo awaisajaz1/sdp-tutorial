71395a92184d4f4e8b44bf4d52b519778dc1646b
awaisajaz1 <awaisajaz1@gmail.com>
2025-12-02 08:09:10 +0000

    Ingestion pipeline is being written

---
+++ transformations/medallion_pipeline.py
@@ -0,0 +1,15 @@
+from pyspark import pipelines as dp
+from pyspark.sql.functions import *
+
+# Materialized View from Source to Bronze
+@dp.materialized_view(
+    comment='Store Bronze Table',
+    name='sdp.bronze.stores'
+)
+def bronze_stores():
+    return (
+        spark.read
+        .table('sdp.source.stores')
+        .withColumn('read_ts', current_timestamp())
+    )
+
